30984479643a5140594622b270d889e029fd5743
Shafiullah Faizi <122133925+shafiafg@users.noreply.github.com>
2026-06-02 18:19:18 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,26 @@
 import tkinter as tk
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+model = genai.GenerativeModel("gemini 1.5 flash")
+
+if not api_key:
+    print("Error: Could not find GEMINI_API_KEY. Check your .env file!")
+else:
+    genai.configure(api_key=api_key)
+    print("AI configured successfully!")
+
+def get_question(difficulty):
+    question_label.config(text="generating question... please wait...")
+    window.update()
+    
+    prompt = f"give me one multiple choice question for AP Computer science principles exa. The difficulty should be {difficulty}. include options A, B, C, and D. also output the correct answer.  " 
+    
+    response = model.generate_content(prompt)
+    question_label.config(text=response.text)
+
 
 
 window = tk.Tk()
@@ -18,8 +39,8 @@ hard_btn.pack(pady=5)
 
 answer_btn = tk.Button(window, text="SHOW ANSWER")
 answer_btn.pack(pady=5)
+api_key=os.getenv("GEMINI_API_KEY")
 
-GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
 window.mainloop()
 
